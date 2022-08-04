b603e58760b6d831a8688fc1b23334c70be90719
ThaTechMaestro <muse.pearls@gmail.com>
2022-08-04 16:56:34 +0100

    Initial Commit

---
+++ src/test/java/com/project/crin/CrinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
